9f9e93fe580b5d06a441ac40c3b5711eccf897d0
cptblackmore <redshatterr@gmail.com>
2025-02-19 17:01:02 +0300

    Improved VacancyFilter ui code & handlers logic, used form components to provide correct values for FormData API, added handleSubmit with for now commented FormData usage boilerplate.

--- src/widgets/VacancyList/ui/VacancyFilter/VacancyFilter.tsx
+++ src/widgets/VacancyList/ui/VacancyFilter/VacancyFilter.tsx
@@ -1,6 +1,6 @@
 import { VacancyParams } from '@entities/Vacancy';
 import { FilterList, FilterListOff } from '@mui/icons-material';
-import { Button, Collapse, Paper, Stack, TextField } from '@mui/material';
+import { Button, Collapse, FormControl, Paper, Stack, TextField } from '@mui/material';
 import { ToggleIconButton } from '@shared/ui';
 import { useVacancyFilter } from '../../model/VacancyFilter/useVacancyFilter';
 import { VacancyFilterAdditional } from './VacancyFilterAdditional';
@@ -12,19 +12,43 @@ interface Props {
 
 export const VacancyFilter: React.FC<Props> = ({ filters, setFilters }) => {
   const { showAdditional, setShowAdditional, state, dispatch } = useVacancyFilter(filters);
+  
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    // const formData = new FormData(e.currentTarget);
+    // const newFilters: VacancyParams['filters'] = {
+    //   text: formData.get('text') as string,
+    //   period: Number(formData.get('period')) as 0 | 1 | 3 | 7,
+    //   salary: {
+    //     from: Number(formData.get('salaryFrom')),
+    //     to: Number(formData.get('salaryTo'))
+    //   }
+    // };
+
+    // setFilters(newFilters);
+
+    setFilters(state);
+  }
 
   return (
-    <Paper sx={{ p: 2, borderRadius: 2, boxShadow: 2, mb: 2 }} >
+    <Paper 
+      sx={{ p: 2, borderRadius: 2, boxShadow: 2, mb: 2 }} 
+      component='form'
+      onSubmit={handleSubmit}
+    >
       <Stack spacing={2} >
         <Stack direction='row' spacing={2} justifyContent='end' >
-          <TextField
-            label='Поиск вакансий'
-            variant='outlined'
-            size='small'
-            fullWidth
-            value={state?.text ?? ''}
-            onChange={(e) => dispatch({type: 'SET_TEXT', text: e.target.value})}
-          />
+          <FormControl fullWidth >
+            <TextField
+              label='Поиск вакансий'
+              name='text'
+              variant='outlined'
+              size='small'
+              value={state?.text ?? ''}
+              onChange={(e) => dispatch({type: 'SET_TEXT', text: e.target.value})}
+            />
+          </FormControl>
           <ToggleIconButton 
             isToggled={showAdditional} 
             onToggle={() => setShowAdditional((prev) => !prev)} 
@@ -33,11 +57,7 @@ export const VacancyFilter: React.FC<Props> = ({ filters, setFilters }) => {
             defaultTooltip='Дополнительные фильтры'
             toggledTooltip='Скрыть дополнительные фильтры'
           />
-          <Button 
-            variant='contained' 
-            color='primary' 
-            onClick={() => setFilters(state)}
-          >
+          <Button variant='contained' color='primary' type='submit' >
             Искать
           </Button>
         </Stack>
--- src/widgets/VacancyList/ui/VacancyFilter/VacancyFilterAdditional.tsx
+++ src/widgets/VacancyList/ui/VacancyFilter/VacancyFilterAdditional.tsx
@@ -1,5 +1,5 @@
 import { VacancyParams } from "@entities/Vacancy";
-import { Box, Stack, Select, MenuItem, Divider, Slider, TextField, Typography as T } from "@mui/material";
+import { Stack, MenuItem, Divider, Slider, TextField, Grid2, FormLabel } from "@mui/material";
 import { Action } from "../../model/VacancyFilter/vacancyFilterReducer";
 
 interface Props {
@@ -8,76 +8,83 @@ interface Props {
 }
 
 export const VacancyFilterAdditional: React.FC<Props> = ({ state, dispatch }) => {
+  function handleSliderChange(newValue: number[]) {
+    dispatch({
+      type: 'HANDLE_SALARY_SLIDER_CHANGE',
+      salary: {from: newValue[0], to: newValue[1]}
+    })
+  }
+
+  const handleSalaryChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: 'from' | 'to'
+  ) => {
+    dispatch({
+      type: field === 'from' ? 'SET_SALARY_FROM' : 'SET_SALARY_TO',
+      salary: {[field]: Number(e.target.value)}
+    });
+  };
+
   return (
-    <Box sx={{ mt: 1, p: 2, border: '1px solid #ddd', borderRadius: 1, boxSizing: 'border-box' }} >
-      <Stack direction='row' >              
-        <Box sx={{ width: '20%'}} >
-          <T gutterBottom >Период размещения</T>
-          <Select 
-            value={state?.period ?? 1} 
+    <Grid2 container mt={2} >
+      <Grid2 size={3} pr={2} >
+        <TextField
+          fullWidth
+          label='Период размещения'
+          name='period'
+          select
+          value={state?.period ?? 1} 
+          size='small'
+          onChange={(e) => dispatch({
+            type: 'SET_PERIOD', 
+            period: Number(e.target.value) as VacancyParams['filters']['period']
+          })} 
+        >
+          <MenuItem value={1} >1 день</MenuItem>
+          <MenuItem value={3} >3 дня</MenuItem>
+          <MenuItem value={7} >7 дней</MenuItem>
+          <MenuItem value={0} >Без ограничения</MenuItem>
+        </TextField>
+      </Grid2>
+
+      <Divider flexItem orientation='vertical' sx={{mr: '-1px'}} />
+      
+      <Grid2 size={9} pl={3} pr={1} >
+        <FormLabel component='legend' sx={{px: 2, fontSize: '0.8em'}} >Диапазон зарплат (₽)</FormLabel>
+        <Slider
+          value={[state?.salary?.from ?? 0, state?.salary?.to ?? 0]}
+          onChange={(_, newValue) => handleSliderChange(newValue as number[])}
+          valueLabelDisplay='auto'
+          min={0}
+          max={200000}
+          step={1000}
+          marks={[
+            { value: 0, label: '0' },
+            { value: 100000, label: '100k' },
+            { value: 200000, label: '200k' },
+          ]}
+        />
+        <Stack direction='row' spacing={2} mt={2} >
+          <TextField
+            label='Мин. зарплата'
+            name='salaryFrom'
+            type='number'
             size='small'
-            fullWidth 
-            onChange={(e) => dispatch({
-              type: 'SET_PERIOD', 
-              period: Number(e.target.value) as VacancyParams['filters']['period']
-            })} 
-          >
-            <MenuItem value={1} >1 день</MenuItem>
-            <MenuItem value={3} >3 дня</MenuItem>
-            <MenuItem value={7} >7 дней</MenuItem>
-            <MenuItem value={0} >Без ограничения</MenuItem>
-          </Select>
-        </Box>
-        <Divider flexItem orientation='vertical' sx={{mx: 2}} />
-        <Box sx={{ width: '80%' }} >
-          <T gutterBottom>Диапазон зарплаты (₽)</T>
-          <Box sx={{px: 2}} >
-            <Slider
-              value={[state?.salary?.from ?? 0, state?.salary?.to ?? 0]}
-              onChange={(e, newValue) => dispatch({
-                type: 'HANDLE_SALARY_SLIDER_CHANGE',
-                salary: {
-                  from: Array.isArray(newValue) ? newValue[0] : newValue, 
-                  to: Array.isArray(newValue) ? newValue[1] : newValue
-                }
-              })}
-              valueLabelDisplay='auto'
-              min={0}
-              max={200000}
-              step={1000}
-              marks={[
-                { value: 0, label: '0' },
-                { value: 100000, label: '100k' },
-                { value: 200000, label: '200k' },
-              ]}
-            />
-          </Box>
-          <Stack direction="row" spacing={2} mt={2} >
-            <TextField
-              label='Мин. зарплата'
-              type='number'
-              size='small'
-              fullWidth
-              value={state?.salary?.from ?? 0}
-              onChange={(e) => dispatch({
-                type: 'SET_SALARY_FROM', 
-                salary: {from: Number(e.target.value)}
-              })}
-            />
-            <TextField
-              label='Макс. зарплата'
-              type='number'
-              size='small'
-              fullWidth
-              value={state?.salary?.to ?? 0}
-              onChange={(e) => dispatch({
-                type: 'SET_SALARY_TO', 
-                salary: {to: Number(e.target.value)}
-              })}
-            />
-          </Stack>
-        </Box>
-      </Stack>
-    </Box>
+            fullWidth
+            value={state?.salary?.from ?? 0}
+            onChange={(e) => handleSalaryChange(e, 'from')}
+          />
+          <TextField
+            label='Макс. зарплата'
+            name='salaryTo'
+            type='number'
+            size='small'
+            fullWidth
+            value={state?.salary?.to ?? 0}
+            onChange={(e) => handleSalaryChange(e, 'to')}
+          />
+        </Stack>
+      </Grid2>
+    </Grid2>
   );
 };
