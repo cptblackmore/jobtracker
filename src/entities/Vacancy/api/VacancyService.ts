51ce6712bc084a7c66519f9ffef826c68af8f8e5
cptblackmore <redshatterr@gmail.com>
2025-03-31 19:19:10 +0300

    Added app id headers in VacancyService via env vars.

--- src/entities/Vacancy/api/VacancyService.ts
+++ src/entities/Vacancy/api/VacancyService.ts
@@ -7,13 +7,17 @@ import { sourcesRegistry } from '../model/sourcesRegistry';
 
 export class VacancyService {
   private static PROXY_URL = import.meta.env.VITE_PROXY_URL;
+  private static SUPERJOB_API_APP_ID = import.meta.env.VITE_SUPERJOB_API_APP_ID;
 
   static async getSuperjob(params: SuperjobParams, signal: AbortSignal): Promise<AxiosResponse<VacancySuperjobMultipleResponse>> {
     return await axios.get(
       this.PROXY_URL ? (this.PROXY_URL + '/superjob/vacancies/') : (sourcesRegistry.superjob.url.api + '/vacancies/'), 
       {
         params,
-        signal
+        signal,
+        headers: {
+          'X-Api-App-Id': this.SUPERJOB_API_APP_ID
+        }
       }
     );
   }
@@ -21,7 +25,12 @@ export class VacancyService {
   static async getSuperjobById(id: string, signal: AbortSignal): Promise<AxiosResponse<VacancySuperjob>> {
     return await axios.get(
       this.PROXY_URL ? (this.PROXY_URL + '/superjob/vacancies/' + id) : (sourcesRegistry.superjob.url.api + '/vacancies/' + id), 
-      { signal }
+      { 
+        signal,
+        headers: {
+          'X-Api-App-Id': this.SUPERJOB_API_APP_ID
+        }
+      }
     );
   }
 
@@ -36,7 +45,12 @@ export class VacancyService {
           return `ids[${i}]=${el}`
         }).join('&')
       ), 
-      { signal }
+      { 
+        signal,
+        headers: {
+          'X-Api-App-Id': this.SUPERJOB_API_APP_ID
+        }
+      }
   );
   }
   
